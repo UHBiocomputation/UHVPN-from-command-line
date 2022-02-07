ddbaf97482872f27c61ea956754e7fd4b5674f98
Michael Schmuker <m.schmuker@biomachinelearning.net>
2022-02-07 23:16:22 +0100

    working

---
+++ README.md
@@ -0,0 +1,13 @@
+# UHVPN from command line
+
+scripting UH VPN access even after UH required authentication via web browser.
+
+To run:
+
+`python connect_UHVPN.py`
+
+## Requirements
+Webdriver-manager, selenium
+
+Get it all via conda/mamba
+`mamba create -n selenium_env selenium webdriver-manager`
---
+++ connect_UHVPN.py
@@ -0,0 +1,51 @@
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import title_is,presence_of_element_located
+from getpass import getpass
+import time
+import os
+import subprocess
+
+service = Service(executable_path=ChromeDriverManager().install())
+options = Options()
+options.page_load_strategy = 'normal'
+driver = webdriver.Chrome(service=service, options=options)
+time.sleep(3)
+driver.get("http://uhvpn.herts.ac.uk") 
+name = driver.find_element(By.NAME,'UserName')
+name.send_keys('ms16aay@herts.ac.uk')
+pw = getpass('password')
+passwdfield = driver.find_element(By.NAME, 'Password')
+passwdfield.send_keys(pw)
+signinbutton = driver.find_element(By.ID, 'submitButton')
+signinbutton.click()
+WebDriverWait(driver,timeout=60).until(title_is("Duo Security"))
+iframe = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.ID,'duo_iframe'))
+driver.switch_to.frame(iframe)
+wrapper = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.CLASS_NAME,'device-select-wrapper'))
+driver.switch_to.default_content()
+iframe = driver.find_element(By.ID, "duo_iframe")
+iframe.send_keys('\n')
+WebDriverWait(driver,timeout=60).until(title_is("Secure Connect Secure - Home"))
+dsid_cookie = driver.get_cookie('DSID')
+print(dsid_cookie['value'])
+driver.quit()
+time.sleep(1)
+print('sudo', 
+	'openconnect', 
+	'-u', 'ms16aay@herts.ac.uk', 
+	'--cookie=DSID={}'.format(dsid_cookie['value']), 
+	'--protocol=nc', 
+	'uhvpn.herts.ac.uk')
+subprocess.run(['sudo', 
+	'openconnect', 
+	'-u', 'ms16aay@herts.ac.uk', 
+	'--cookie=DSID={}'.format(dsid_cookie['value']), 
+	'--protocol=nc', 
+	'uhvpn.herts.ac.uk'])
